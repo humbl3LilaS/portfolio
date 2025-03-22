3882dbde13dd25d309940b84b6bb2d364371480c
humbl3LilaS <sabishinekobebe@gmail.com>
2025-03-22 14:41:16 +0630

    add: projects section

---
+++ src/assets/constants.ts
@@ -0,0 +1,27 @@
+export const PROJECTS = [
+    {
+        title: "MyanTech",
+        tags: ["nextJS", "typescript", "drizzle", "neon", "Shadcn UI"],
+        repoUrl: "https://github.com/humbl3LilaS/team-ikigai",
+        liveUrl: "https://kumo-liart.vercel.app/",
+        coverImage: "/src/assets/project-images/myan-tech.png",
+        description:
+            "An award winning project at Code2Career Hackathon 2024 organized by JICA Myanmar Office",
+    },
+    {
+        title: "Kumo",
+        tags: ["react", "typescript", "appwrite"],
+        repoUrl: "https://github.com/humbl3LilaS/kumo",
+        liveUrl: "https://kumo-liart.vercel.app/",
+        coverImage: "/src/assets/project-images/kumo.png",
+        description: "A social media app where user can share amazing day with friend and family.",
+    },
+    {
+        title: "Feedback Board",
+        tags: ["ReactJS", "TypeScript", "Supabase"],
+        repoUrl: "https://github.com/humbl3LilaS/feedback-board",
+        liveUrl: "https://feedback-board-hazel.vercel.app/",
+        coverImage: "/src/assets/project-images/feedback-board.png",
+        description: "A feedback board where user can share feedback about the product",
+    },
+];
